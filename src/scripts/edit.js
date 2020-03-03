d4f2cd291ce563e89cf98100e8e517f592d7cb35
xry <yxruisoft@sina.com>
2020-03-03 20:05:45 +0800

    优化脚本实现

--- src/scripts/edit.js
+++ src/scripts/edit.js
@@ -2,17 +2,23 @@ var fs = require('fs');
 var plist = require('plist');
 var xml2js = require('xml2js');
 
+const appKey = 'EmasAppKey';
+const appSecret = 'EmasAppSecret';
+const bundleId = 'EmasBundleId';
+
 module.exports = function (ctx) {
 
-    var filePath = ctx.opts.projectRoot + '/plugins/' + ctx.opts.plugin.id + '/src/AliyunEmasServices-Info.plist';
+    if (!ctx.opts.platforms.includes('ios')) {
+        return;
+    }
     var configPath = ctx.opts.projectRoot + '/config.xml';
-    // console.log('-------->',ctx.opts.platforms.includes('android'))
-    var xml = fs.readFileSync(filePath, 'utf8');
-    var obj = plist.parse(xml);
     var configXML = fs.readFileSync(configPath, 'utf8');
     var xmlParser = new xml2js.Parser({explicitArray: false, ignoreAttrs: false})
     var preferences = {}
+    var projectName = '';
     xmlParser.parseString(configXML, function (err, result) {
+        projectName = result.widget.name;
+        console.log('--------> ', projectName);
         var json = result.widget.platform[1].preference;
         for (var i = 0; i < json.length; i++) {
             var key = json[i].$.name
@@ -20,10 +26,12 @@ module.exports = function (ctx) {
             preferences[key] = value;
         }
     })
-
-    obj.config['emas.appKey'] = preferences['EmasAppKey'] || ''
-    obj.config['emas.appSecret'] = preferences['EmasAppSecret'] || ''
-    obj.config['emas.bundleId'] = preferences['EmasBundleId'] || ''
+    var filePath = ctx.opts.projectRoot + '/platforms/ios/' + projectName + '/Resources/AliyunEmasServices-Info.plist';
+    var xml = fs.readFileSync(filePath, 'utf8');
+    var obj = plist.parse(xml);
+    obj.config['emas.appKey'] = preferences[appKey] || ''
+    obj.config['emas.appSecret'] = preferences[appSecret] || ''
+    obj.config['emas.bundleId'] = preferences[bundleId] || ''
     console.log(obj);
 
     xml = plist.build(obj);
