c39b1996337e14195511da534ced6f720bf94d6f
xry <yxruisoft@sina.com>
2020-03-03 18:26:51 +0800

    优化脚本；增加注释

--- src/scripts/edit.js
+++ src/scripts/edit.js
@@ -1,14 +1,15 @@
 var fs = require('fs');
 var plist = require('plist');
 var xml2js = require('xml2js');
-var FILEPATH = './plugins/cordova-plugin-aliyun-emas-analytics/src/AliyunEmasServices-Info.plist';
-var CONFIGPATH = './config.xml';
 
 module.exports = function (ctx) {
 
-    var xml = fs.readFileSync(FILEPATH, 'utf8');
+    var filePath = ctx.opts.projectRoot + '/plugins/' + ctx.opts.plugin.id + '/src/AliyunEmasServices-Info.plist';
+    var configPath = ctx.opts.projectRoot + '/config.xml';
+    // console.log('-------->',ctx.opts.platforms.includes('android'))
+    var xml = fs.readFileSync(filePath, 'utf8');
     var obj = plist.parse(xml);
-    var configXML = fs.readFileSync(CONFIGPATH, 'utf8');
+    var configXML = fs.readFileSync(configPath, 'utf8');
     var xmlParser = new xml2js.Parser({explicitArray: false, ignoreAttrs: false})
     var preferences = {}
     xmlParser.parseString(configXML, function (err, result) {
@@ -23,10 +24,8 @@ module.exports = function (ctx) {
     obj.config['emas.appKey'] = preferences['EmasAppKey'] || ''
     obj.config['emas.appSecret'] = preferences['EmasAppSecret'] || ''
     obj.config['emas.bundleId'] = preferences['EmasBundleId'] || ''
-    // console.log(obj);
+    console.log(obj);
 
     xml = plist.build(obj);
-    fs.writeFileSync(FILEPATH, xml, {encoding: 'utf8'});
-    // fs.writeFileSync('./www/AliyunEmasServices-Info.plist', xml, {encoding: 'utf8'});
-
+    fs.writeFileSync(filePath, xml, {encoding: 'utf8'});
 };
--- www/XRPreference.js
+++ www/XRPreference.js
@@ -2,23 +2,54 @@ var exec = require('cordova/exec');
 
 var XRPreference = {
 
-    registerData:function(args, success, error) {
+    /**
+     * @brief 初始化准备数据
+     * @details 优先调用，否则可能会出现意想不到的bug
+     */
+    registerData: function (args, success, error) {
         exec(success, error, 'XRPreference', 'registerData', args);
     },
 
-    initAlicloudAPM:function () {
+    /**
+     * @brief 性能监控初始化接口（自动读取appKey、appSecret）
+     * @details 性能监控初始化接口，appKey、appSecret会从AliyunEmasServices-Info.plist自动读取
+     */
+    initAlicloudAPM: function () {
         exec(null, null, 'XRPreference', 'initAlicloudAPM', null);
     },
-    initAlicloudTlog:function () {
+
+    /**
+     * @brief 远程日志初始化接口（自动读取appKey、appSecret）
+     * @details 远程日志初始化接口，appKey、appSecret会从AliyunEmasServices-Info.plist自动读取
+     */
+    initAlicloudTlog: function () {
         exec(null, null, 'XRPreference', 'initAlicloudTlog', null);
     },
-    initAlicloudCrash:function () {
+
+    /**
+     * @brief 崩溃分析始化接口（自动读取appKey、appSecret）
+     * @details 崩溃分析初始化接口，appKey、appSecret会从AliyunEmasServices-Info.plist自动读取
+     */
+    initAlicloudCrash: function () {
         exec(null, null, 'XRPreference', 'initAlicloudCrash', null);
     },
+
+    /**
+     * @brief 启动AppMonitor服务
+     * @details 启动AppMonitor服务，可包括崩溃分析、远程日志、性能监控
+     */
     start: function () {
         exec(null, null, 'XRPreference', 'start', null);
     },
-    autoStartAliyunAnalyticsWithArgs:function (args, success, error) {
+
+    /**
+     @brief 自动启动阿里云服务：<性能分析&远程日志&崩溃分析>
+     自动读取appKey、appSecret
+     只需单独集成此api即可
+     启动服务需在config.xml中配置，具体参照文档说明
+     函数会返回失败信息，成功无返回
+     */
+    autoStartAliyunAnalyticsWithArgs: function (args, success, error) {
         exec(success, error, 'XRPreference', 'autoStartAliyunAnalyticsWithArgs', args);
     }
 };
